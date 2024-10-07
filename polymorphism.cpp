decf628cd7d84fed35fc15b235026108d5fa87cb
Bhanu-222 <115560445+Bhanu-222@users.noreply.github.com>
2024-10-07 13:03:52 +0530

    Polymorphism in OOPS

---
+++ polymorphism.cpp
@@ -0,0 +1,9 @@
+/* polymorphism is the ability of objects to take different form or behave in different 
+ways depending on the context in which they are used.
+Two types: 1. Compile time polymorphism - constructor overloading
+		   2. Run time polymorphism*/
+
+#include<iostream>
+using namespace std;
+
+
